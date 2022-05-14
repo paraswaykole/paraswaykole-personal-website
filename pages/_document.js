57865bf80d2bf21a7e3dc55bc71547292d71550d
Paras Waykole <paraswaykole12@gmail.com>
2022-05-14 14:10:19 +0530

    init website

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class AppDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta content="charset=UTF-8" />
+          <meta name="viewport" content="width=device-width" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <div className="appcontainer">
+            <Main />
+            <NextScript />
+          </div>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default AppDocument;
