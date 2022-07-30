e0bfe997843092d69d834c883fd247a32e13e42c
Paras Waykole <paraswaykole12@gmail.com>
2022-07-30 21:48:58 +0530

    update about

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,8 @@ export default function Home() {
           </p>
           <p>
             Building and making things has always been my favourite thing to do.
-            I have been programming since the age of 12 (built ~5 2D PC games). 
-            Later, started a mobile gaming company where I created around 6 Mobile Game titles.
+            I started building 2D PC games since the age of 12 (built ~5 PC games). 
+            Later in 2014, started a mobile gaming company where I created around 6 Mobile Game titles.
           </p>
           <p>
             In 2016, I started building Superbot, which was an AI
