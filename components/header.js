1efe6d45f35f5932273c3b88b164545f7047a27d
Paras Waykole <paraswaykole12@gmail.com>
2023-09-23 19:54:47 +0530

    update profile photo

--- components/header.js
+++ components/header.js
@@ -8,7 +8,7 @@ export default function Header() {
         <div>
           <img
             alt="paras waykole"
-            src="paras-waykole.png"
+            src="paras-waykole.jpg"
             className="avatar"
             height="100"
             width="100"
